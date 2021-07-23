7cc58470102a464f1ef0309fc8525b44caed55fe
jaskiniowiec135 <patryk.niklewicz135@gmail.com>
2021-07-23 18:21:00 +0200

    added converter for boolean values

---
+++ FairDivision/Converters/OppositeBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace FairDivision.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class OppositeBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool?))
+            {
+                throw new InvalidOperationException("The target must be a nullable boolean");
+            }
+            bool? b = (bool?)value;
+            return b.HasValue && !b.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return !(value as bool?);
+        }
+    }
+}
